567a401a203eb9b683a0a0b12614aafca0a00ff4
Asif419 <asshah419@gmail.com>
2023-04-10 01:43:55 +0600

    category data and category component starting

---
+++ src/components/JobCategories/JobCategories.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const JobCategories = () => {
+  return (
+    <div className='category-container'>
+      <div className='text-center'>
+        <p className='text-4xl font-semibold pt-8 md:pt-14 pb-4'>Job Category List</p>
+        <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+      </div>
+    </div>
+  );
+};
+
+export default JobCategories;
