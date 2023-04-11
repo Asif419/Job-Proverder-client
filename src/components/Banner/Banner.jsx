3c94735faa39c82431626d28c88216cd0b917733
Asif419 <asshah419@gmail.com>
2023-04-12 01:13:08 +0600

    picture added in banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Banner = ({children}) => {
   return (
-    <div className='text-center bg-gradient-to-b from-indigo-50 to-gray-50 h-[15vh] md:h-[20vh] flex justify-center items-center'>
+    <div className='text-center bg-gradient-to-b from-indigo-50 to-indigo-50 h-[15vh] md:h-[20vh] flex justify-center items-center relative'>
+      <img className='absolute object-cover left-0 w-[40%] md:w-[15%] bottom-0' src="Vector.png" alt="" />
+      <img className='absolute object-cover right-0 w-[40%] md:w-[15%] -top-20' src="Vector-1.png" alt="" />
       <p className='font-bold text-lg'>{children}</p>
     </div>
   );
